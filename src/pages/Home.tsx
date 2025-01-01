d1de7aa2e3c9a2adc62aa076bdb328f355f3756e
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-01 15:45:58 +0530

    linkedin social added

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import { Github } from "../icons/SocialIcons/Github"
+import { Linkedin } from "../icons/SocialIcons/Linkedin"
 import { Upwork } from "../icons/SocialIcons/Upwork"
 
 export const Home = () => {
@@ -10,9 +11,16 @@ export const Home = () => {
                 <div className="text-custom-1 text-4xl">I'm a <span className="font-bold">Fullstack Developer</span></div> <br />
                 <div className="text-custom-1 text-2xl">I got expertise in Building & Deploying <br /> <span className="font-bold">Backend</span> Applications & API's </div>
 
-                <div className="flex gap-4 mt-6 cursor-pointer">
-                    <Github/>
-                    <Upwork/>
+                <div className="flex gap-4 mt-6">
+                    <div onClick={() => {window.open("https://github.com/Shubhashish-Chakraborty")}} className="cursor-pointer">
+                        <Github/>
+                    </div>
+                    <div onClick={() => {window.open("https://www.upwork.com/freelancers/~018880ca3d0d892eac")}} className="cursor-pointer">
+                        <Upwork/>
+                    </div>
+                    <div onClick={() => {window.open("https://www.linkedin.com/in/Shubhashish-Chakraborty/")}} className="cursor-pointer">
+                        <Linkedin/>
+                    </div>
                 </div>
 
                 <div className="mt-6">
