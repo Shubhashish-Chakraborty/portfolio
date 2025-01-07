2c5abffde63e409bc3a00315f59aadf978364af7
Shubhashish-Chakraborty <shubhashish147@gmail.com>
2025-01-07 23:51:46 +0530

    bg fixed

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -3,7 +3,7 @@ import { ProjectBox } from "../components/ProjectBox.tsx";
 export const Project = () => {
     return (
         <>  
-            <div className="flex justify-center flex-wrap">
+            <div className="flex justify-center flex-wrap bg-blue-50">
                 <ProjectBox title="Tech Learning PlatForm" thumbnail="https://www.studywithshubh.tech/swsBrandImg.png" githubLink="https://github.com/Shubhashish-Chakraborty/StudyWithShubh"  webLink="https://www.studywithshubh.tech/" description="some description i will add later"/>
                 <ProjectBox title="Real-Time ChatApp" thumbnail="https://convoroom.vercel.app/convoRoomLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/convoroom"  webLink="https://convoroom.vercel.app" description="some description i will add later"/>
                 <ProjectBox title="The ExtraOrdinary URL Shortener" thumbnail="https://theshortlink.vercel.app/tslLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/TheShortLink"  webLink="https://theshortlink.vercel.app" description="some description i will add later"/>
