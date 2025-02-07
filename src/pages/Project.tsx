6a7bc4c8105fba3f5242d33e5c06c0fe6c42717a
Shubhashish Chakraborty <164302071+Shubhashish-Chakraborty@users.noreply.github.com>
2025-02-07 22:03:47 +0530

    project added

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -7,6 +7,7 @@ export const Project = () => {
                 <ProjectBox title="Tech Learning PlatForm" thumbnail="https://www.studywithshubh.tech/swsBrandImg.png" githubLink="https://github.com/Shubhashish-Chakraborty/StudyWithShubh"  webLink="https://www.studywithshubh.tech/" description="some description i will add later"/>
                 <ProjectBox title="Real-Time ChatApp" thumbnail="https://convoroom.vercel.app/convoRoomLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/convoroom"  webLink="https://convoroom.vercel.app" description="some description i will add later"/>
                 <ProjectBox title="The ExtraOrdinary URL Shortener" thumbnail="https://theshortlink.vercel.app/tslLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/TheShortLink"  webLink="https://theshortlink.vercel.app" description="some description i will add later"/>
+                <ProjectBox title="Private Data Manager" thumbnail="https://myprivdata.vercel.app/myPrivData_Logo.png" githubLink="https://github.com/Shubhashish-Chakraborty/myprivdata"  webLink="https://myprivdata.vercel.app" description="some description i will add later"/>
                 <ProjectBox title="File Handling Utility" thumbnail="https://pypi.org/static/images/logo-small.8998e9d1.svg" githubLink="https://github.com/Shubhashish-Chakraborty/pyfilemaster"  webLink="https://pypi.org/project/pyfilemaster/" description="some description i will add later"/>
             </div>
         </>
