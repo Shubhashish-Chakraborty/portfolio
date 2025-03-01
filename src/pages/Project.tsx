a667efb175daa4dd3101fbc4c692bfbfbe614e3d
Shubhashish Chakraborty <164302071+Shubhashish-Chakraborty@users.noreply.github.com>
2025-03-02 01:20:02 +0530

    Changes made in projects

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -9,7 +9,7 @@ export const Project = () => {
                 <ProjectBox title="Real-Time ChatApp" thumbnail="https://convoroom.vercel.app/convoRoomLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/convoroom"  webLink="https://convoroom.vercel.app" />
                 <ProjectBox title="The ExtraOrdinary URL Shortener" thumbnail="https://theshortlink.vercel.app/tslLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/TheShortLink"  webLink="https://theshortlink.vercel.app" />
                 <ProjectBox title="Private Data Manager" thumbnail="https://myprivdata.vercel.app/myPrivData_Logo.png" githubLink="https://github.com/Shubhashish-Chakraborty/myprivdata"  webLink="https://myprivdata.vercel.app" />
-                <ProjectBox title="College Practical Code Solutions" thumbnail="https://bcecodes.vercel.app/webLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/bce-code-solution"  webLink="https://bcecodes.vercel.app" />
+                <ProjectBox title="Code Solutions For Students" thumbnail="https://ipcode.vercel.app/webLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/bce-code-solution"  webLink="https://ipcode.vercel.app" />
                 <ProjectBox title="File Handling Utility" thumbnail="https://pypi.org/static/images/logo-small.8998e9d1.svg" githubLink="https://github.com/Shubhashish-Chakraborty/pyfilemaster"  webLink="https://pypi.org/project/pyfilemaster/" />
             </div>
         </>
