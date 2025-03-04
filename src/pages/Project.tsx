8d61d6066dabf5550801adcc5ba9b27dbb9e9955
Shubhashish Chakraborty <164302071+Shubhashish-Chakraborty@users.noreply.github.com>
2025-03-05 00:08:18 +0530

    typo fixed in infino url

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -5,7 +5,7 @@ export const Project = () => {
         <>  
             <div className="flex justify-center flex-wrap bg-blue-50">
                 <ProjectBox title="Tech Learning PlatForm" thumbnail="https://www.studywithshubh.tech/swsBrandImg.png" githubLink="https://github.com/Shubhashish-Chakraborty/StudyWithShubh"  webLink="https://www.studywithshubh.tech/" />
-                <ProjectBox title="Infino || E-Commerce Platform" thumbnail="https://infinostore.com/infinoLogo.png" githubLink="https://github.com/infinostore"  webLink="https://nfinostore.com" />
+                <ProjectBox title="Infino || E-Commerce Platform" thumbnail="https://infinostore.com/infinoLogo.png" githubLink="https://github.com/infinostore"  webLink="https://infinostore.com" />
                 <ProjectBox title="Real-Time ChatApp" thumbnail="https://convoroom.vercel.app/convoRoomLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/convoroom"  webLink="https://convoroom.vercel.app" />
                 <ProjectBox title="The ExtraOrdinary URL Shortener" thumbnail="https://theshortlink.vercel.app/tslLogo.png" githubLink="https://github.com/Shubhashish-Chakraborty/TheShortLink"  webLink="https://theshortlink.vercel.app" />
                 <ProjectBox title="Private Data Manager" thumbnail="https://myprivdata.vercel.app/myPrivData_Logo.png" githubLink="https://github.com/Shubhashish-Chakraborty/myprivdata"  webLink="https://myprivdata.vercel.app" />
